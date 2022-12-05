ab4ada73ffca92e5a2d296677d157b4a54e9e4d5
Joshua Little <jplittle2001@gmail.com>
2022-12-05 01:31:13 -0500

    update

--- 2022/tropic-lang/src/main.rs
+++ 2022/tropic-lang/src/main.rs
@@ -1,28 +1,48 @@
+mod eval;
 mod lexer;
 
+use clap::{Parser, Subcommand};
+use std::fs;
 use std::path::PathBuf;
-
-use clap::Parser;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// program to be run
-    #[arg(index = 1, value_name = "PROGRAM")]
-    program: Option<PathBuf>,
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// executes the program provided
+    Run {
+        #[arg(value_name = "PROGRAM")]
+        program: PathBuf,
+    },
+
+    /// formats the program provided
+    Format {
+        #[arg(value_name = "PROGRAM")]
+        program: PathBuf,
+    },
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    // if let Some(program) = cli.program.as_deref() {
-    //     println!(
-    //         "Value for name: {}",
-    //         program.to_str().expect("problem with path")
-    //     );
-    // }
+    match &cli.command {
+        Some(Commands::Run { program }) => {
+            let program_string = fs::read_to_string(program).unwrap();
+            let tokens = lexer::tokenize(program_string.as_str());
+            let ast = lexer::parse(&tokens);
 
-    lexer::tokenize("this is a test")
+            println!("{:?}", ast);
+        }
+        Some(Commands::Format { program }) => {
+            let program_string = fs::read_to_string(program).unwrap();
+            let output = lexer::format(program_string.as_str());
+            println!("\"\"\"\n{}\n\"\"\"", output);
+        }
+        None => {}
+    }
 }
-
-fn run_program() {}
